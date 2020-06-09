4405d4b6b6ed2887b20b5e95aad7af9ea07aa8a3
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2020-06-09 16:49:20 +0300

    Adds products in database

--- db.sql
+++ db.sql
@@ -69,8 +69,29 @@ CREATE TABLE orders
     total_price int                not NULL
 );
 
+INSERT INTO supplier (name, description)
+VALUES ('Amazon', 'Digital content and services.');
+INSERT INTO supplier (name, description)
+VALUES ('Lenovo', 'Computers');
+INSERT INTO supplier (name, description)
+VALUES ('Ebay', 'All you can think of');
 
+INSERT INTO category (name, department, description)
+VALUES ('Tablets', 'Hardware',
+        'A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.');
+INSERT INTO category (name, department, description)
+VALUES ('Smartwatch', 'Wearable', 'Fitness Tracker with Heart Rate Monitor.');
 
-
-
+INSERT INTO product (name, price, currency, image, description, supplier_id, category_id)
+VALUES ('Amazon Fire', 49.9, 'USD', 'product_1.jpg', 'Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.', 1, 1);
+INSERT INTO product (name, price, currency, image, description, supplier_id, category_id)
+VALUES ('Lenovo IdeaPad Miix 700', 479, 'USD', 'product_2.jpg', 'Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.', 2, 1);
+INSERT INTO product (name, price, currency, image, description, supplier_id, category_id)
+VALUES ('Amazon Fire HD 8', 89, 'USD', 'product_3.jpg', 'Latest Fire HD 8 tablet is a great value for media consumption.', 1, 1);
+INSERT INTO product (name, price, currency, image, description, supplier_id, category_id)
+VALUES ('Apple T80', 230, 'USD', 'product_4.jpg', 'Waterproof smartwatch with a square design with a 1.4-inch, LCD display.', 3, 2);
+INSERT INTO product (name, price, currency, image, description, supplier_id, category_id)
+VALUES ('Realme', 49, 'USD', 'product_5.jpg', '20 day battery life smartwatch with interchangeable straps of different colors', 3, 2);
+INSERT INTO product (name, price, currency, image, description, supplier_id, category_id)
+VALUES ('Sony', 69, 'USD', 'product_6.jpg', 'Sony SE20 yellow smartwatch with interchangeable strap.', 3, 2);
 
