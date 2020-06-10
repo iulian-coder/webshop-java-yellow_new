517ba5ef5f41d9fc3ae5e097d55004bdba5fcc93
Andreea Grosu <andreea.grosu87@gmail.com>
2020-06-10 22:57:22 +0300

    Add doPost method for registration data to be saved in the DB

--- src/main/java/com/codecool/shop/controller/RegistrationController.java
+++ src/main/java/com/codecool/shop/controller/RegistrationController.java
@@ -1,14 +1,12 @@
 package com.codecool.shop.controller;
 
-import com.codecool.shop.dao.CartDao;
-import com.codecool.shop.dao.ProductCategoryDao;
-import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.*;
 
-import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.dao.implementation.*;
 import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.model.Product;
 
+import com.codecool.shop.model.User;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 import com.codecool.shop.model.ProductCategory;
@@ -36,10 +34,46 @@ public class RegistrationController extends HttpServlet {
 
     }
 
-//    @Override
-//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//
-//
-//    }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        UserDao userDao = null;
+
+        try {
+            userDao = UserDaoJDBC.getInstance();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        String firstName = req.getParameter("first_name");
+        String lastName = req.getParameter("last_name");
+        String username = req.getParameter("username");
+        String email = req.getParameter("email");
+        String phone = req.getParameter("phone_number");
+        String password = req.getParameter("password");
+        String billingAddress = req.getParameter("billing_address");
+        String shippingAddress = req.getParameter("shipping_address");
+
+        User user =new User(username, password, firstName, lastName, phone, email, billingAddress, shippingAddress);
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(username);
+        System.out.println(email);
+        System.out.println(phone);
+        System.out.println(password);
+        System.out.println(billingAddress);
+        System.out.println(shippingAddress);
+
+        try {
+            userDao.add(user);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("New user added");
+
+        resp.sendRedirect("/login");
+
+    }
 
 }
