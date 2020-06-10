590e58f7bcacc804f00189b46cafd1c692ffcf01
Andreea Grosu <andreea.grosu87@gmail.com>
2020-06-10 21:32:31 +0300

    Add register and login forms and controller classes with doGet method

---
+++ src/main/java/com/codecool/shop/controller/LoginController.java
@@ -0,0 +1,46 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.dao.CartDao;
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+
+import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.dao.implementation.*;
+import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.model.Product;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import com.codecool.shop.model.ProductCategory;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@WebServlet(urlPatterns = {"/login"})
+public class LoginController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+        engine.process("login.html", context, resp.getWriter());
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.sendRedirect("/");
+
+    }
+
+}
